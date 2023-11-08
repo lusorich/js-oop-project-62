280d61522b92440b1b106a5468bddc6d9c7ce18e
dreamonaut <dreamonautre@gmail.com>
2023-11-08 16:21:33 +0500

    init and some code

--- src/schema/BaseSchema.js
+++ src/schema/BaseSchema.js
@@ -2,12 +2,6 @@ class BaseSchema {
   constructor() {
     this.validators = [];
   }
-
-  string() {
-    this.currentSchema = new this.schemas.String();
-
-    return this.currentSchema;
-  }
 }
 
 export default BaseSchema;
