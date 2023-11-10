4babc8603c8e945de02ca8e0e9dbde299c0ff418
dreamonaut <dreamonautre@gmail.com>
2023-11-10 13:33:26 +0500

    add contains validator

--- src/schema/BaseSchema.js
+++ src/schema/BaseSchema.js
@@ -2,6 +2,23 @@ class BaseSchema {
   constructor() {
     this.validators = [];
   }
+
+  isValid(value) {
+    for (let i = 0; i < this.validators.length; i += 1) {
+      const { fn, args = null } = this.validators[i];
+      const isValidByValidator = fn(value, args);
+
+      if (!isValidByValidator) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  setValidator(validator) {
+    this.validators.push(validator);
+  }
 }
 
 export default BaseSchema;
--- src/schema/StringSchema.js
+++ src/schema/StringSchema.js
@@ -3,30 +3,24 @@ import BaseSchema from './BaseSchema.js';
 const test = {
   required: (value) => !!value,
   minLength: (value, minLength) => value.length >= minLength,
+  contains: (value, substring) => value.split(' ').includes(substring),
 };
 
 class StringSchema extends BaseSchema {
-  isValid(value) {
-    for (let i = 0; i < this.validators.length; i += 1) {
-      const { fn, args = null } = this.validators[i];
-      const isValidByValidator = fn(value, args);
-
-      if (!isValidByValidator) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
   required() {
-    this.validators.push({ fn: test.required });
+    this.setValidator({ fn: test.required });
 
     return this;
   }
 
   minLength(minLength = 0) {
-    this.validators.push({ fn: test.minLength, args: minLength });
+    this.setValidator({ fn: test.minLength, args: minLength });
+
+    return this;
+  }
+
+  contains(substring) {
+    this.setValidator({ fn: test.contains, args: substring });
 
     return this;
   }
