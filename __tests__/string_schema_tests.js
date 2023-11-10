e3aa34c867206120dea33f5d8e8e9b04c8216f6e
dreamonaut <dreamonautre@gmail.com>
2023-11-10 13:53:48 +0500

    Adding tests for string schema

---
+++ __tests__/string_schema_tests.js
@@ -0,0 +1,49 @@
+import Validator from '../src/Validator.js';
+import StringSchema from '../src/schema/StringSchema.js';
+
+let validator;
+let schema;
+
+beforeEach(() => {
+  validator = new Validator();
+  schema = validator.string();
+});
+
+describe('string schema test', () => {
+  test('string method return StringSchema class', () => {
+    expect(schema).toBeInstanceOf(StringSchema);
+  });
+
+  test("if required value can't be null or undefined or empty string", () => {
+    schema.required();
+
+    expect(schema.isValid()).toBeFalsy();
+    expect(schema.isValid(null)).toBeFalsy();
+    expect(schema.isValid(undefined)).toBeFalsy();
+    expect(schema.isValid('null')).toBeTruthy();
+  });
+
+  test("if minLength added value length can't be less", () => {
+    schema.minLength(3);
+
+    expect(schema.isValid('12')).toBeFalsy();
+    expect(schema.isValid('1234')).toBeTruthy();
+
+    schema.minLength(5);
+
+    expect(schema.isValid('1234')).toBeFalsy();
+    expect(schema.isValid('12345')).toBeTruthy();
+  });
+
+  test('if contains added value must contain a substring', () => {
+    schema.contains('mother');
+
+    expect(schema.isValid('father fffmother')).toBeFalsy();
+    expect(schema.isValid('father mother')).toBeTruthy();
+
+    schema.contains('father');
+
+    expect(schema.isValid('father mother')).toBeTruthy();
+    expect(schema.isValid('cousin mother')).toBeFalsy();
+  });
+});
